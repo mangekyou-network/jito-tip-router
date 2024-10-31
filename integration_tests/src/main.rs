9fb8a4a3f006d3fda12dca68c02f4faab384c674
Christian Krueger <christian@jito.wtf>
2024-10-31 11:41:58 -0600

    making sure client gen is okay

--- integration_tests/src/main.rs
+++ integration_tests/src/main.rs
@@ -1,11 +1,5 @@
-// use jito_restaking_client::instructions::{
-//     InitializeConfigBuilder, InitializeNcnBuilder, InitializeOperatorBuilder,
-// };
-
-// use jito_weight_table_client::types::Weight;
-// use jito_weight_table_client::programs::JITO_WEIGHT_TABLE_ID;
+use jito_mev_tip_distribution_ncn_client::programs::JITO_MEV_TIP_DISTRIBUTION_NCN_ID;
 
 pub fn main() {
-    // let weight = Weight::new(1, 2).unwrap();
-    // println!("Hello, world! {:?}", JITO_WEIGHT_TABLE_ID);
+    println!("Hello, world! {:?}", JITO_MEV_TIP_DISTRIBUTION_NCN_ID);
 }
